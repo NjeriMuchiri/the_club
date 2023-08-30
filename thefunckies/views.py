971187a078b09cffa01d19ce304ce2a7e1d318ea
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-30 23:20:58 +0300

    adding the calender to our home page dynamically

--- thefunckies/views.py
+++ thefunckies/views.py
@@ -4,7 +4,7 @@ from calendar import HTMLCalendar
 from datetime import datetime
 
 # Create your views here.
-def home(request, year, month):
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
     #converting month to number
     month = month.capitalize()
     month_number = list(calendar.month_name).index(month)
@@ -15,9 +15,12 @@ def home(request, year, month):
     #Get the current year
     now = datetime.now()
     current_year = now.year
+    #Get the current time
+    time = now.strftime('%I:%M:%S %p')
     context = {"year": year,
                "month": month,
                "month_number": month_number,
                "the_calendar": the_calendar,
-               "current_year": current_year}
+               "current_year": current_year,
+               "time": time}
     return render(request, 'thefunckies/home.html', context)
