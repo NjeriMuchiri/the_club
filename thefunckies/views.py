d6e672d7796bbbc99deff85e8a8d2d424f25f7c2
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-30 11:55:23 +0300

    adding the calender to our home page

--- thefunckies/views.py
+++ thefunckies/views.py
@@ -4,10 +4,16 @@ from calendar import HTMLCalendar
 
 # Create your views here.
 def home(request, year, month):
+    
     #converting month to number
     month = month.capitalize()
     month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    
+    #craete calender
+    the_calendar = HTMLCalendar().formatmonth(year, month_number)
     context = {"year": year,
                "month": month,
-               "month_number": month_number}
+               "month_number": month_number,
+               "the_calender": the_calendar}
     return render(request, 'thefunckies/home.html', context)
